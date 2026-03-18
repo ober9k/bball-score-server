6db7481daa5eef47d58678e959b2bbd2644afa84
Dean Hardaker <ober9k@users.noreply.github.com>
2026-03-19 02:19:34 +1100

    Added prisma and related config/generated files

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ This can be configured as desired, but the base values should be as such:
 ```
 PORT="8080"
 ```
+### Postgres Settings
+This can be configured as desired, however the `docker-compose` file just has some filler values for the `user` and `secret` used for the connection.
+```
+DATABASE_URL="postgresql://postgres:secret@localhost:5432/bball_score?schema=public"
+```
---
+++ prisma/migrations/20260318151743_initial_migration/migration.sql
@@ -0,0 +1,74 @@
+-- CreateEnum
+CREATE TYPE "Position" AS ENUM ('POINT_GUARD', 'SHOOTING_GUARD', 'SMALL_FORWARD', 'POWER_FORWARD', 'CENTER');
+
+-- CreateTable
+CREATE TABLE "League" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "League_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Season" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "leagueId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Season_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Division" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "seasonId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Division_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "shortName" TEXT NOT NULL,
+    "divisionId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Player" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "position" "Position" NOT NULL,
+    "number" TEXT NOT NULL,
+    "height" TEXT NOT NULL,
+    "teamId" INTEGER NOT NULL,
+
+    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "League_slug_key" ON "League"("slug");
+
+-- AddForeignKey
+ALTER TABLE "Season" ADD CONSTRAINT "Season_leagueId_fkey" FOREIGN KEY ("leagueId") REFERENCES "League"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Division" ADD CONSTRAINT "Division_seasonId_fkey" FOREIGN KEY ("seasonId") REFERENCES "Season"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Team" ADD CONSTRAINT "Team_divisionId_fkey" FOREIGN KEY ("divisionId") REFERENCES "Division"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Player" ADD CONSTRAINT "Player_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
