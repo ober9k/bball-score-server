521c10ac36faa79bc897ce779b21b5531a14e508
Dean Hardaker <ober9k@users.noreply.github.com>
2026-03-27 16:07:57 +1100

    Added index for routes

---
+++ src/routes/index.ts
@@ -0,0 +1,24 @@
+import { authRoutes } from "@/routes/auth.routes";
+import { divisionsRoutes } from "@/routes/divisions.routes";
+import { playersRoutes } from "@/routes/player.routes";
+import { seasonsRoutes } from "@/routes/seasons.routes";
+import { standingsRoutes } from "@/routes/standings.routes";
+import { statisticsRoutes } from "@/routes/statistics.routes";
+import { teamsRoutes } from "@/routes/teams.routes";
+
+export const routes = [
+  authRoutes,
+];
+
+export const leagueRoutes = [
+  divisionsRoutes,
+  playersRoutes,
+  seasonsRoutes,
+  standingsRoutes,
+  statisticsRoutes,
+  teamsRoutes,
+];
+
+export const manageRoute = [
+  /* TBD */
+];
