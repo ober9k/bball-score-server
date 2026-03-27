441b54ed22b28f4f4e1b41d72c724297a4ce6c05
Dean Hardaker <ober9k@users.noreply.github.com>
2026-03-27 16:08:26 +1100

    Added league handler for middleware pass through

---
+++ src/middlewares/league-handler.ts
@@ -0,0 +1,23 @@
+import { findLeague, setLocalLeague } from "@/services/league.service";
+import type { NextFunction, Request, Response } from "express";
+
+/**
+ * All non /auth URLs are tied to a league, hence have this as a pre-requisite for all URLs.
+ */
+export async function leagueHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  // this should work off an ID or a slug
+  const leagueId = +req.params.leagueId;
+
+  // store locally for subsequent requests
+  const league = await findLeague(leagueId);
+
+  if (league) {
+    setLocalLeague(res, league); // otherwise exception
+  }
+
+  next();
+}
