1412d58c7c34c7945ccc36d13b24a11fa10d3447
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-11 20:34:08 +1000

    Added endpoint for /me to retrieve current user

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
-import { login, logout } from "@/controllers/auth.controller";
+import { login, logout, me } from "@/controllers/auth.controller";
 import { isAuthenticated } from "@/middlewares/auth-token";
 import { Router } from "express";
 
 const router = Router()
   .post("/auth/login", login)
   .get("/auth/logout", [isAuthenticated], logout)
+  .get("/auth/me", [isAuthenticated], me)
 
 export { router as authRoutes };
