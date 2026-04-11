7cbed60a99c656f88e61481cd63721c624a511ce
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-11 20:26:00 +1000

    Updated error messages for login request

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,16 +5,30 @@ import * as bcrypt from "bcryptjs";
 import { type Request, type Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
+function buildJsonSuccess(status: number, message: string) {
+  return {
+    status,
+    message,
+  };
+}
+
+function buildJsonError(status: number, message: string, formErrors: string[]) {
+  return {
+    status,
+    message,
+    formErrors,
+  };
+}
+
 export async function login(req: Request, res: Response) {
   try {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({
-        status: "error",
-        msg: "No credentials",
-        errors: [{msg: "No credentials"}]
-      });
+      return res.status(StatusCodes.UNAUTHORIZED)
+        .json(
+          buildJsonError(StatusCodes.UNAUTHORIZED, "Error", ["Please fill in all required fields."])
+        );
     }
 
     const user = await prisma.user.findUnique({
@@ -22,11 +36,10 @@ export async function login(req: Request, res: Response) {
     }) as any;
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({
-        status: 'error',
-        msg: 'Invalid credentials',
-        errors: [{ msg: "Invalid credentials" }]
-      });
+      return res.status(StatusCodes.UNAUTHORIZED)
+        .json(
+          buildJsonError(StatusCodes.UNAUTHORIZED, "Error", ["Please enter the correct email address and password combination."])
+        );
     }
 
     // generate user token
@@ -45,27 +58,33 @@ export async function login(req: Request, res: Response) {
     });
   }
   catch (error) {
-    console.error(error);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      status: "error",
-      error: error.message,
-      msg: "Internal server error."
-    });
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(
+        buildJsonError(StatusCodes.INTERNAL_SERVER_ERROR, "Error", ["Internal server error."])
+      );
   }
 }
 
 export async function logout(req: Request, res: Response) {
   // ideally, the token should be cleared too?
   if (req.cookies[AuthCookieKey]) {
-    res
+    return res
       .clearCookie(AuthCookieKey)
-      .status(200)
-      .json({
-        message: "You have logged out"
-      })
+      .status(StatusCodes.OK)
+      .json(
+        buildJsonSuccess(StatusCodes.OK, "Successfully logged out.")
+      );
   }
 
-  res.status(StatusCodes.UNAUTHORIZED).json({
-    error: "Invalid token"
-  });
+  return res
+    .status(StatusCodes.UNAUTHORIZED)
+    .json(
+      buildJsonError(StatusCodes.UNAUTHORIZED, "Error", ["Invalid token provided."])
+    );
+}
+
+export async function me(req: Request, res: Response) {
+  return res
+    .status(StatusCodes.OK)
+    .json(req.user);
 }
