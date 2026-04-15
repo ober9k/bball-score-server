017c8ec3fe0f0ce3d9d3c29c00d694ad580c962f
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-16 02:58:05 +1000

    Adjusted stats structure to suit other future usages

--- src/controllers/games.controller.ts
+++ src/controllers/games.controller.ts
@@ -15,9 +15,47 @@ export async function getGames(req: Request, res: Response) {
 }
 
 export async function getGame(req: Request, res: Response) {
+
+  const toStats = (gtp) => ({
+    seconds:        gtp.seconds,
+    fgMade:         gtp.fgMade,
+    fgAttempted:    gtp.fgAttempted,
+    fg3Made:        gtp.fg3Made,
+    fg3Attempted:   gtp.fg3Attempted,
+    ftMade:         gtp.ftMade,
+    ftAttempted:    gtp.ftAttempted,
+    points:         gtp.points,
+    offRebounds:    gtp.offRebounds,
+    defRebounds:    gtp.defRebounds,
+    rebounds:       gtp.rebounds,
+    assists:        gtp.assists,
+    steals:         gtp.steals,
+    blocks:         gtp.blocks,
+    turnovers:      gtp.turnovers,
+    personalFouls:  gtp.personalFouls,
+    technicalFouls: gtp.technicalFouls,
+  });
+
+  /* a bit ugly for now */
   return res
     .status(StatusCodes.OK)
     .json(
       await findGameById(getGameId(req))
+        .then((game) => {
+          return {
+            ...game,
+            gameTeams: [
+              ...game.gameTeams.map((gt) => ({
+                ...gt,
+                gameTeamPlayers: gt.gameTeamPlayers
+                  .map((gtp) => ({
+                    player:  gtp.player,
+                    started: gtp.started,
+                    stats:   toStats(gtp),
+                  }))
+              })),
+            ],
+          }
+        })
     );
 }
