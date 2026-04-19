4ae14bc428978aa187d2e1ab6f5374fef28a8056
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-19 16:04:42 +1000

    Added played and forfeits for standings

--- src/types/standings-log.ts
+++ src/types/standings-log.ts
@@ -3,10 +3,12 @@ import type { Team } from "@/types/team.ts";
 export type StandingsLog = {
   id:            number,
   team:          Team,
+  played:        number,
   wins:          number,
   losses:        number,
   draws:         number,
   byes:          number,
+  forfeits:      number,
   pointsFor:     number,
   pointsAgainst: number,
 };
--- src/utils/standings-utils.ts
+++ src/utils/standings-utils.ts
@@ -7,24 +7,28 @@ function generateStandingsLog(log: any): StandingsLog {
   const { team } = log;
   const { id } = team;
 
+  const played        = 0;
   const wins          = 0;
   const losses        = 0;
   const draws         = 0;
   const byes          = 0;
+  const forfeits      = 0;
   const pointsFor     = 0;
   const pointsAgainst = 0;
 
-  return { id, team, wins, losses, draws, byes, pointsFor, pointsAgainst };
+  return { id, team, played, wins, losses, draws, byes, forfeits, pointsFor, pointsAgainst };
 }
 
 /**
  * Accumulate standings for a game based on the provided team.
  */
 export function accumulateForGame(log: StandingsLog, scoreA: number, scoreB): void {
+  log.played        += 1; /* increment for any log, later factor forfeit */
   log.wins          += +(scoreA > scoreB);
   log.losses        += +(scoreA < scoreB);
   log.draws         += +(scoreA === scoreB);
   log.byes          += 0; /* tbd */
+  log.forfeits      += 0; /* tbd */
   log.pointsFor     += scoreA;
   log.pointsAgainst += scoreB;
 }
