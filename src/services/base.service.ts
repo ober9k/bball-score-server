4a58fea8ced1640c7c25ac98628b51bba09d83f3
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-30 21:15:56 +1000

    Updated base service to include generic find and save methods

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -1,17 +1,69 @@
 import { toOption } from "@/lib/converters";
 import type { Option } from "@/types/option";
 
-export abstract class BaseService<T, TBrief, TBriefData> {
+/**
+ * This is a work in progress and likely a repository class can instead be used
+ * for some of this functionality.
+ */
+export abstract class BaseService<T, TBrief, TBriefData, TDelegate, TSelect, TOrderByWithRelationInput> {
 
-  public abstract async findAll(brief?: boolean): Promise<T[] | TBrief[]>;
+  public abstract getDelegate(): TDelegate;
 
-  public abstract async findById(id: number, brief?: boolean): Promise<T | TBrief>;
+  public async findAll(brief?: boolean): Promise<T[] | TBrief[]> {
+    const items: any[] = await this.getDelegate().findMany({
+      select:  (brief)
+        ? this.getBriefSelectColumns()
+        : this.getSelectColumns(),
+      orderBy: this.getOrderByColumns(),
+    });
 
-  public abstract async save(data: TBriefData): Promise<TBrief>;
+    return (brief)
+      ? this.mapBriefItems(items)
+      : this.mapItems(items);
+  }
 
-  public abstract async saveById(id: number, data: TBriefData): Promise<TBrief>;
+  public async findById(id: number, brief?: boolean): Promise<T | TBrief> {
+    const item: any = await this.getDelegate().findUniqueOrThrow({
+      select: (brief)
+        ? this.getBriefSelectColumns()
+        : this.getSelectColumns(),
+      where:  { id },
+    });
 
-  public abstract async findOptions(): Promise<Option[]>;
+    return (brief)
+      ? this.toBriefItem(item)
+      : this.toItem(item);
+  }
+
+  public async save(data: TBriefData): Promise<TBrief> {
+    const { activated, saveData } = data;
+
+    const item: any = await this.getDelegate().create({
+      data: { ...saveData, active: activated }, /* temp: transform structure */
+    });
+
+    return this.findById(item.id, true);
+  }
+
+  public async saveById(id: number, data: TBriefData): Promise<TBrief> {
+    const { activated, saveData } = data;
+
+    const item: any = await this.getDelegate().update({
+      data: { ...saveData, active: activated }, /* temp: transform structure */
+      where: { id },
+    });
+
+    return this.findById(item.id, true);
+  }
+
+  public async findOptions(): Promise<Option[]> {
+    const items: any[] = this.getDelegate().findMany({
+      select:  { id: true, name: true },
+      orderBy: this.getOrderByColumns(),
+    });
+
+    return this.mapOptions(items);
+  }
 
   protected abstract toItem(data: any): T;
 
@@ -35,5 +87,11 @@ export abstract class BaseService<T, TBrief, TBriefData> {
     return items
       .map((item) => this.toOption(item));
   }
+
+  protected abstract getSelectColumns(): TSelect;
+
+  protected abstract getBriefSelectColumns(): TSelect;
+
+  protected abstract getOrderByColumns(): TOrderByWithRelationInput;
 
 }
