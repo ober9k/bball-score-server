d1696c5a39aa034a6acbefbaca871bbefac9053d
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-30 17:59:24 +1000

    Added new season service class and applied to controller

--- src/controllers/seasons.controller.ts
+++ src/controllers/seasons.controller.ts
@@ -1,6 +1,6 @@
 import { getLocalLeague } from "@/services/league.service";
-import { findAll, findAllAsOptions, findBriefAll, findBriefById, findById, findDivisionsBySeasonId, save, saveById } from "@/services/season.service";
-import type { SeasonData } from "@/types/season";
+import { findDivisionsBySeasonId, SeasonService } from "@/services/season.service";
+import type { BriefSeasonData } from "@/types/season";
 import type { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
@@ -8,12 +8,12 @@ function getSeasonId(req: Request): number {
   return +req.params.id;
 }
 
-function getSeasonData(req: Request, res: Response): SeasonData {
-  const { name, active, archived } = req.body;
+function getSeasonData(req: Request, res: Response): BriefSeasonData {
+  const { name, activated, archived } = req.body;
   const { id: leagueId } = getLocalLeague(res);
 
   return {
-    name, active, archived, leagueId,
+    name, activated, archived,
   };
 }
 
@@ -21,7 +21,7 @@ export async function getSeasons(req: Request, res: Response) {
   return res
     .status(StatusCodes.OK)
     .json(
-      await findAll()
+      await (new SeasonService()).findAll()
     );
 }
 
@@ -29,7 +29,7 @@ export async function getSeason(req: Request, res: Response) {
   return res
     .status(StatusCodes.OK)
     .json(
-      await findById(getSeasonId(req))
+      await (new SeasonService()).findById(getSeasonId(req))
     );
 }
 
@@ -37,7 +37,7 @@ export async function getSeasonsOptions(req: Request, res: Response) {
   return res
     .status(StatusCodes.OK)
     .json(
-      await findAllAsOptions()
+      await (new SeasonService()).findOptions()
     );
 }
 
@@ -45,7 +45,7 @@ export async function getBriefSeasons(req: Request, res: Response) {
   return res
     .status(StatusCodes.OK)
     .json(
-      await findBriefAll()
+      await (new SeasonService()).findAll(true),
     );
 }
 
@@ -53,7 +53,7 @@ export async function getBriefSeason(req: Request, res: Response) {
   return res
     .status(StatusCodes.OK)
     .json(
-      await findBriefById(getSeasonId(req))
+      await (new SeasonService()).findById(getSeasonId(req), true)
     );
 }
 
@@ -61,7 +61,7 @@ export async function createSeason(req: Request, res: Response) {
   return res
     .status(StatusCodes.CREATED)
     .json(
-      await save(getSeasonData(req, res))
+      await (new SeasonService()).save(getSeasonData(req, res))
     );
 }
 
@@ -69,12 +69,12 @@ export async function updateSeason(req: Request, res: Response) {
   return res
     .status(StatusCodes.OK)
     .json(
-      await saveById(getSeasonId(req), getSeasonData(req, res))
+      await (new SeasonService()).saveById(getSeasonId(req), getSeasonData(req, res))
     );
 }
 
 export async function getSeasonDivisions(req: Request, res: Response) {
-  await findById(getSeasonId(req)); /* trigger an initial failure if not found */
+  await (new SeasonService()).findById(getSeasonId(req)); /* trigger an initial failure if not found */
 
   return res
     .status(StatusCodes.OK)
--- src/services/season.service.ts
+++ src/services/season.service.ts
@@ -1,8 +1,9 @@
-import { toBriefSeason, toDivision, toOption, toSeason } from "@/lib/converters";
+import { toBriefSeason, toDivision, toSeason } from "@/lib/converters";
 import { prisma } from "@/lib/prisma";
+import { BaseService } from "@/services/base.service";
 import type { Division } from "@/types/division";
 import type { Option } from "@/types/option";
-import type { BriefSeason, Season, SeasonData } from "@/types/season";
+import type { BriefSeason, BriefSeasonData, Season, SeasonData } from "@/types/season";
 import { SortOrder } from "@prisma/generated/internal/prismaNamespace";
 import type { SeasonOrderByWithRelationInput, SeasonSelect } from "@prisma/generated/models/Season";
 
@@ -34,57 +35,72 @@ function defaultOrderBy(): SeasonOrderByWithRelationInput {
   };
 }
 
-export async function _findAll(brief?: boolean): Promise<any[]> {
-  return prisma.season.findMany({
-    select:  (brief)
-      ? briefSelect()
-      : defaultSelect(),
-    orderBy: defaultOrderBy(),
-  });
-}
+export class SeasonService extends BaseService<Season, BriefSeason, BriefSeasonData>{
 
-export async function findAll(): Promise<Season[]> {
-  const items: any[] = await _findAll();
+  public async findAll(brief?: boolean): Promise<Season[] | BriefSeason[]> {
+    const items: any[] = await prisma.season.findMany({
+      select:  (brief)
+        ? briefSelect()
+        : defaultSelect(),
+      orderBy: defaultOrderBy(),
+    });
 
-  return items
-    .map(toSeason);
-}
+    return (brief)
+      ? this.mapBriefItems(items)
+      : this.mapItems(items);
+  }
 
-export async function findBriefAll(): Promise<BriefSeason[]> {
-  const items: any[] = await _findAll(true);
+  public async findById(id: number, brief?: boolean): Promise<Season | BriefSeason> {
+    const item: any = await prisma.season.findUniqueOrThrow({
+      select: (brief)
+        ? briefSelect()
+        : defaultSelect(),
+      where:  { id },
+    });
 
-  return items
-    .map(toBriefSeason);
-}
+    return (brief)
+      ? this.toBriefItem(item)
+      : this.toItem(item);
+  }
 
-async function _findById(id: number, brief?: boolean): Promise<any> {
-  return prisma.season.findUniqueOrThrow({
-    select: (brief)
-      ? briefSelect()
-      : defaultSelect(),
-    where:  { id },
-  });
-}
+  public async save(data: BriefSeasonData): Promise<BriefSeason> {
+    const { activated, saveData } = data;
 
-export async function findById(id: number): Promise<Season | null> {
-  const item: any = await _findById(id);
+    const item: any = await prisma.season.create({
+      data: { ...saveData, active: activated }, /* temp: transform structure */
+    });
 
-  return (item)
-    ? toSeason(item)
-    : null;
-}
+    return this.findById(item.id, true);
+  }
 
-export async function findBriefById(id: number): Promise<BriefSeason | null> {
-  const item: any = await _findById(id, true);
+  public async saveById(id: number, data: BriefSeasonData): Promise<BriefSeason> {
+    const { activated, saveData } = data;
 
-  return (item)
-    ? toBriefSeason(item)
-    : null;
-}
+    const item: any = await prisma.season.update({
+      data: { ...saveData, active: activated }, /* temp: transform structure */
+      where: { id },
+    });
 
-export async function findAllAsOptions(): Promise<Option[]> {
-  return (await findAll())
-    .map(toOption);
+    return this.findById(item.id, true);
+  }
+
+  public async findOptions(): Promise<Option[]> {
+    const items: any[] = await prisma.season.findMany({
+      select:  { id: true, name: true },
+      orderBy: defaultOrderBy(),
+    });
+
+    return this.mapOptions(items);
+  }
+
+  protected toItem(data: any): Season {
+    return toSeason(data);
+  }
+
+  protected toBriefItem(data: any): BriefSeason {
+    return toBriefSeason(data);
+  }
+
 }
 
 export async function save(data: SeasonData): Promise<Season | null> {
