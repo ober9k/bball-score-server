3d26c2afb59ca1b8dddfe00641ac7afa89eb4d55
Dean Hardaker <ober9k@users.noreply.github.com>
2026-05-01 23:16:29 +1000

    Added base controller to try and simplify stuff

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -1,4 +1,5 @@
-import type { Response } from "express";
+import { getLocalLeague } from "@/services/league.service";
+import type { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
 export function ok<T>(res: Response, data: T) {
@@ -11,4 +12,18 @@ export function created<T>(res: Response, data: T) {
   return res
     .status(StatusCodes.CREATED)
     .json(data);
+}
+
+export abstract class BaseController<TBriefData> {
+
+  protected getId(req: Request): number {
+    return +req.params.id;
+  }
+
+  protected getLeagueId(res: Response): number {
+    return getLocalLeague(res).id;
+  }
+
+  protected abstract getBriefData(req: Request, res: Response): TBriefData;
+
 }
