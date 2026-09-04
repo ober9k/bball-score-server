a9dc9896c3d920c3e1eee413f624239681598270
Dean Hardaker <ober9k@users.noreply.github.com>
2026-09-04 16:54:41 +1000

    Updated cors usage for origin and credentials

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,10 @@ import passport from "passport";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
